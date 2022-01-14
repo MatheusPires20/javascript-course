fa82481fca808c08bff7eff842ed7ec77ba8e1cf
Matheus Pires <97488645+MatheusPires20@users.noreply.github.com>
2022-01-13 22:51:17 -0300

    estudo sobre a variável LET e como usá-la

---
+++ aula sobre variáveis (LET)/scripts.js
@@ -0,0 +1,10 @@
+console.log('Hello World!'); // O console. log é usado para aparecer no console (terminal) o conteúdo exigido. ('Hello World') //
+console.log('Wellcome!');
+let years = 20;  // O let é uma variável que pode aceitar diversos valores //
+let height = 182;
+let city = 'Londrina'; // Quando se fala no conteúdo das lets, quando for um texto, elas devem ser escritas entre aspas simples //
+let name = 'Matheus';
+console.log(name); // Aqui, e nas senguntes funções, ele está pegando as informações descrevidas e imprimindo na tela de console //
+console.log(years);
+console.log(height);
+console.log(city);
